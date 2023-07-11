03110a597c08f9360fdb4cf900a6a98e5908ca43
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-07-11 14:13:47 +0600

    add fungiton

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useMemo, useState } from "react";
 
 interface DatePickerProps {
   value: Date;
@@ -6,8 +6,82 @@ interface DatePickerProps {
   min?: Date;
   max?: Date;
 }
+const months = [
+  "January",
+  "Febriary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "Jule",
+  "August",
+  "Seprember",
+  "Octomber",
+  "November",
+  "December",
+];
+interface DataCellItem {
+  day: number;
+  month: number;
+  year: number;
+}
+const getDaysAmountInMonth = (year: number, month: number) => {
+  const nextMonthDay = new Date(year, month + 1, 1);
+  //mutates the date object
+  nextMonthDay.setMinutes(-1);
+  return nextMonthDay.getDate();
+};
 export const DatePicker = ({ value, onChange, min, max }: DatePickerProps) => {
-  return <div>DatePicker</div>;
+  const [panelYear] = useState(() => value.getFullYear());
+  const [panelMonth] = useState(() => value.getMonth());
+  const [year, month, day] = useMemo(() => {
+    const currentYear = value.getFullYear();
+    const currentDay = value.getDate();
+    const currentMonth = months[value.getMonth()];
+    return [currentDay, currentMonth, currentYear];
+  }, [value]);
+
+  const getPrevMonthDays = () => {};
+
+  const getNextMonthDays = () => {};
+  
+  const getCurrentMonthDays = (
+    year: number,
+    month: number,
+    numberOfDays: number
+  ) => {
+    const dateCells: DataCellItem[] = [];
+    for (let i = 1; i < numberOfDays; i++) {
+      dateCells.push({ year, month, day: i });
+    }
+    return dateCells;
+  };
+  const dateCells = useMemo(() => {
+    const items: DataCellItem[] = [];
+    const daysInAMonth = getDaysAmountInMonth(panelYear, panelMonth);
+    console.log(daysInAMonth);
+    return items;
+  }, [panelMonth, panelYear]);
+
+  const onDateSelect = () => {};
+
+  const nextYear = () => {};
+
+  const prevYear = () => {};
+
+  const nextMonth = () => {};
+
+  const prevMonth = () => {};
+
+  return (
+    <div>
+      <div>
+        {day}
+        {month}
+        {year}
+      </div>
+    </div>
+  );
 };
 
 export default DatePicker;
